c874588f9f40bf7554a7de517dac0cecd5e6e9a2
unknown <joseph.harley02@gmail.com>
2023-01-11 17:21:32 -0500

    getCity function can now grab the correct object from the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
-var btnEl = document.querySelector("#searchBtn");
+var searchBtnEl = document.querySelector("#searchBtn");
 var cityEl = document.querySelector("#city-search");
+var apiKey = "72a59ebfae3893a4cb8b37a32f9526af";
 
-btnEl.addEventListener("click", function(event) {
+
+searchBtnEl.addEventListener("click", function(event) {
     event.preventDefault();
 
-    console.log(cityEl.value);
+    getCity(cityEl.value);    
 });
+
+
+function getCity(city) {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + apiKey;
+
+    fetch(apiUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+function getWeather(lat, lon) {
+    var apiUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey;
+}
